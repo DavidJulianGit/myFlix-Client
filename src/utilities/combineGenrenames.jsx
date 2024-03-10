5b2f1f959bdb3fe0f0a4320e14c8edaa726beaee
DavidJulianGit <DavidJulianGit@users.noreply.github.com>
2024-03-10 16:26:27 +0100

    Deleting proptypes from combineGenrenames and import react in movie-ivew

--- src/utilities/combineGenrenames.jsx
+++ src/utilities/combineGenrenames.jsx
@@ -2,11 +2,3 @@
 export default function combineGenreNames(genres) {
    return genres.map((genre) => genre.name).join(', ');
 }
-
-combineGenreNames.PropTypes = {
-   genres: PropTypes.arrayOf(
-      PropTypes.shape({
-         name: PropTypes.string.isRequired,
-      })
-   ).isRequired,
-};
