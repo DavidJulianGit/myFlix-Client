0cb3ff377ccd0751aaec93742a608957e92d6477
DavidJulianGit <DavidJulianGit@users.noreply.github.com>
2024-03-10 16:23:05 +0100

    Update combineGenrenames.jsx

--- src/utilities/combineGenrenames.jsx
+++ src/utilities/combineGenrenames.jsx
@@ -1,11 +1,4 @@
-import PropTypes from 'prop-types';
 
-/**
- * Combines an array of genre objects into a single string of genre names, separated by commas.
- *
- * @param {Array<Object>} genres - An array of genre objects, where each object has a 'name' property.
- * @returns {string} - A string containing the concatenated genre names separated by commas.
- */
 export default function combineGenreNames(genres) {
    return genres.map((genre) => genre.name).join(', ');
 }
